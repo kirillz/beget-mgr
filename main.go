0090f765ffde2ba0453f3977b9223f7720c923fd
Kirill aka IceFOX <kzasorin@gmail.com>
2023-10-01 07:07:45 +0300

    fix

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"github.com/kirillz/beget-mgr/v3/internal/ui"
+	"github.com/kirillz/beget-mgr/internal/assets"
+	"github.com/kirillz/beget-mgr/internal/ui"
 )
 
 func main() {
 	a := app.New()
+	assets.SetIcon(a)
 	w := a.NewWindow("Beget-mgr v0.0.1")
 	w.Resize(fyne.NewSize(700, 400))
+
 	w.SetMaster()
 
 	w.SetContent(ui.Create(a, w))
